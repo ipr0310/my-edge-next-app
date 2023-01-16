79f59766e0033f537389ac85c866e94e0429e7cf
ipr0310 <35440957+ipr0310@users.noreply.github.com>
2023-01-15 23:39:08 -0400

    Format response as JSON

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,4 +4,16 @@ export const config = {
   runtime: "edge",
 };
 
-export default (req: NextRequest) => new Response("Hello world!");
+export default async function handler(req: NextRequest) {
+  const response = {
+    text: "Hello World!",
+    secondText: "Hola mundo!",
+  };
+
+  return new Response(JSON.stringify(response), {
+    status: 200,
+    headers: {
+      "content-type": "application/json",
+    },
+  });
+}
